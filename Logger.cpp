d6c089f1a2213e34b8e821d2ac1cd71842b7db75
sephirothbahamut <sephirothbahamut@live.it>
2019-09-10 19:09:21 +0200

    solved issues

--- Logger.cpp
+++ Logger.cpp
@@ -2,48 +2,47 @@
 #include "Logger.h"
 
 void Logger::writer()
+	{
+	while (running)
 		{
-		while (running)
-			{
-			std::unique_lock<std::mutex> lock(queue_free);
-			work_available.wait(lock);
-
-			queue_write.swap(queue_log);
-
-			lock.unlock();
-
-			write_all();
-			}
-		}
-
-	void Logger::write_all()
-		{
-		while (!queue_write.empty()) { log_file << queue_write.front() << std::endl; queue_write.pop(); }
-		}
-
-	void Logger::log(std::string string)
-		{
-
-		queue_free.lock();
-		queue_log.push(string);
-
-		queue_free.unlock();
-
-		work_available.notify_one();
-		}
-
-	Logger::Logger() : Logger("log.txt") {}
-	Logger::Logger(std::string fname = "log.txt") : thread(&Logger::writer, this)
-		{
-		log_file.open(fname);
-		}
-	Logger::~Logger()
-		{
-		log("Logger | Flushing logs after engine end");
-		running = false;
-		thread.join();
+		std::unique_lock<std::mutex> lock(queue_free);
+		work_available.wait(lock);
 
 		queue_write.swap(queue_log);
+
+		lock.unlock();
+
 		write_all();
-		log_file.close();
 		}
+	}
+
+void Logger::write_all()
+	{
+	while (!queue_write.empty()) { log_file << queue_write.front() << std::endl; queue_write.pop(); }
+	}
+
+void Logger::log(std::string string)
+	{
+	std::unique_lock<std::mutex> lock(queue_free);
+	queue_log.push(string);
+
+	lock.unlock();
+
+	work_available.notify_one();
+	}
+
+Logger::Logger() : Logger("log.txt") {}
+Logger::Logger(std::string fname = "log.txt") : thread(&Logger::writer, this)
+	{
+	log_file.open(fname);
+	}
+Logger::~Logger()
+	{
+	log("Logger | Flushing logs after engine end");
+	running = false;
+	thread.join();
+
+	queue_write.swap(queue_log);
+	write_all();
+	log_file.close();
+	}
--- Logger.h
+++ Logger.h
@@ -7,24 +7,24 @@
 #include <mutex>
 
 class Logger
-		{
-		private:
-			std::ofstream log_file;
+	{
+	private:
+		std::ofstream log_file;
 
-			std::queue<std::string> queue_log;
-			std::queue<std::string> queue_write;
-			std::thread thread;
+		std::queue<std::string> queue_log;
+		std::queue<std::string> queue_write;
+		std::thread thread;
 
-			std::condition_variable work_available;
-			std::mutex queue_free;
-			std::atomic<bool> running = true;
+		std::condition_variable work_available;
+		std::mutex queue_free;
+		std::atomic<bool> running = true;
 
-			void writer();
-			void write_all();
+		void writer();
+		void write_all();
 
-		public:
-			void log(std::string string);
-			Logger();
-			Logger(std::string fname);
-			~Logger();
-		};
+	public:
+		void log(std::string string);
+		Logger();
+		Logger(std::string fname);
+		~Logger();
+	};
